42fdf29c61676843baea06968116f815d723d790
Paolo <paolo.gines@gmail.com>
2025-03-06 19:22:36 +0800

    Created model for Recipe

--- ledger/models.py
+++ ledger/models.py
@@ -7,3 +7,9 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
