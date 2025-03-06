4d17011b79496af1509f4fffe38cc134fad3ac6d
Paolo <paolo.gines@gmail.com>
2025-03-07 00:09:48 +0800

    Changed recipes view to class based

--- ledger/views.py
+++ ledger/views.py
@@ -1,73 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.detail import DetailView
 from .models import Recipe
 
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipe_list.html', {'recipes': recipes})
 
-def recipes(request, num):
-    recipe = {}
-    if num == 1:
-        recipe = {
-            "name": "Recipe 1",
-            "ingredients": [
-                {
-                    "name": "tomato",
-                    "quantity": "3pcs"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1kg"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1L"
-                },
-                {
-                    "name": "sinigang mix",
-                    "quantity": "1 packet"
-                }
-            ],
-            "link": "/recipe/1"
-        }
-    elif num == 2:
-        recipe = {
-            "name": "Recipe 2",
-            "ingredients": [
-                {
-                    "name": "garlic",
-                    "quantity": "1 head"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "vinegar",
-                    "quantity": "1/2cup"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1 cup"
-                },
-                {
-                    "name": "salt",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "whole black peppers",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1 kilo"
-                }
-            ],
-            "link": "/recipe/2"
-        }
-    return render(request, "recipe.html", recipe)
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = 'recipe.html'
+    context_object_name = 'recipe'
