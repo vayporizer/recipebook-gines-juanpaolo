48a9f86ca0798adf1b35af7a40f70c764522867f
Paolo <paolo.gines@gmail.com>
2025-03-06 22:20:12 +0800

    Implemented absolute urls

--- ledger/models.py
+++ ledger/models.py
@@ -1,20 +1,26 @@
 from django.db import models
-
-# Create your models here.
+from django.urls import reverse
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+    def get_absolute_url(self):
+        return reverse('ingredient_detail', args=[str(self.name)])
     
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
     
     def __str__(self):
         return self.name
+    def get_absolute_url(self):
+        return reverse('recipe_detail', args=[str(self.name)])
     
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=10)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="ingredients")
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="recipe")
+    
+    def __str__(self):
+        return '{} of {} in {}'.format(self.quantity, self.ingredient.name, self.recipe.name)
