d13e1096bb306074c641a320f5477df1974f452b
Paolo <paolo.gines@gmail.com>
2025-03-19 00:27:22 +0800

    Created Profile model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
@@ -24,3 +25,11 @@ class RecipeIngredient(models.Model):
     
     def __str__(self):
         return '{} of {} in {}'.format(self.quantity, self.ingredient.name, self.recipe.name)
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(max_length=255)
+
+    def __str__(self):
+        return self.name
