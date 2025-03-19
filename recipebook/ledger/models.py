e7678859ceb10da9b330e4f3cd8b0fe696e04375
Paolo <paolo.gines@gmail.com>
2025-03-19 21:11:45 +0800

    Changed author field to foreign key

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -20,7 +20,7 @@ class Ingredient(models.Model):
     
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
-    author = models.OneToOneField(Profile, on_delete=models.CASCADE, default=None)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, default=None)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     
