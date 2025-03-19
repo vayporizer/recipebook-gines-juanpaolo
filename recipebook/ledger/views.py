d76ceedd49c4a1b3d840daf4a488042528727574
Paolo <paolo.gines@gmail.com>
2025-03-19 21:39:30 +0800

    Added login requirement for views

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Recipe
 
+@login_required
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipe_list.html', {'recipes': recipes})
 
+@login_required
 def recipe(request, key):
     recipe = Recipe.objects.get(id=key)
     return render(request, 'recipe.html', {'recipe': recipe})
