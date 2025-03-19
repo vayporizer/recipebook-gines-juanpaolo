8a82f5664dfb39c482d00af806e67f791601c171
Paolo <paolo.gines@gmail.com>
2025-03-19 12:20:12 +0800

    Created Profile admin

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Ingredient, Recipe, RecipeIngredient
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Ingredient, Recipe, RecipeIngredient, Profile
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
@@ -23,10 +25,19 @@ class RecipeAdmin(admin.ModelAdmin):
 
 class RecipeIngredientAdmin(admin.ModelAdmin):
     model = RecipeIngredient
-    list_display = ('recipe', 'ingredient', 'quantity')  
+    list_display = ('recipe', 'ingredient', 'quantity')
     search_fields = ('recipe__name', 'ingredient__name')
-    list_filter = ('recipe', 'ingredient')  
+    list_filter = ('recipe', 'ingredient')
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline,]
 
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
