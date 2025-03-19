9bd011fd5a764f575a5ed986ecadf417a635f561
Paolo <paolo.gines@gmail.com>
2025-03-19 21:32:19 +0800

    Changed recipe detail view to function based for consistency

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from django.views.generic.detail import DetailView
 from .models import Recipe
 
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipe_list.html', {'recipes': recipes})
 
-class RecipeDetailView(DetailView):
-    model = Recipe
-    template_name = 'recipe.html'
-    context_object_name = 'recipe'
+def recipe(request, key):
+    recipe = Recipe.objects.get(id=key)
+    return render(request, 'recipe.html', {'recipe': recipe})
