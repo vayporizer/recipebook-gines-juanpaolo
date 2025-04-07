3f30b5661e7fbf9d6b021e51f422d0d3eb039abc
Paolo <paolo.gines@gmail.com>
2025-04-07 19:51:22 +0800

    Created path for recipe maker

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import recipe_list, recipe
+from .views import recipe_list, recipe, recipe_maker
 
 urlpatterns = [
     path('recipes/list/', recipe_list, name='recipe_list'),
-    path('recipe/<int:key>/', recipe, name='recipe')
+    path('recipe/<int:key>/', recipe, name='recipe'),
+    path('recipe/add/', recipe_maker, name='recipe_maker')
 ]
 
 app_name = 'ledger'
