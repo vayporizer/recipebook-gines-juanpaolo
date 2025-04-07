877489fde05ba4abb4d66dca4ddae03666637c54
Paolo <paolo.gines@gmail.com>
2025-04-07 20:11:37 +0800

    Created IngredientForm

--- recipebook/ledger/forms.py
+++ recipebook/ledger/forms.py
@@ -1,7 +1,11 @@
 from django import forms
-from .models import Recipe
-
+from .models import Recipe, Ingredient
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = ['name']
+
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = '__all__'
