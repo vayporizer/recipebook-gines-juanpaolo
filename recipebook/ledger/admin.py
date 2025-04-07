b7b8f0339468b934b3e0460b6b92ed6cb07c8c78
Paolo <paolo.gines@gmail.com>
2025-04-07 13:10:34 +0800

    Added RecipeImages to Recipe admin

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import Ingredient, Recipe, RecipeIngredient, Profile
+from .models import Ingredient, Recipe, RecipeIngredient, Profile, RecipeImage
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
+
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
 
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
@@ -14,7 +17,7 @@ class IngredientAdmin(admin.ModelAdmin):
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
-    inlines = [RecipeIngredientInline]
+    inlines = [RecipeIngredientInline, RecipeImageInline]
     ('Details', {'ingredients':('name', 'quantity')})
     list_display = ('name', 'author', 'created_on', 'updated_on')
     search_fields = ('name',)
