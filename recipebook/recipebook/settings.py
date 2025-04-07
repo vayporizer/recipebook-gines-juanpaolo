320b1e5aa364ce29d6db0f961bb6799e9b02e28a
Paolo <paolo.gines@gmail.com>
2025-04-07 22:14:40 +0800

    Modified project settings, urls, and base template to use static files

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_ROOT = BASE_DIR/'media'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
