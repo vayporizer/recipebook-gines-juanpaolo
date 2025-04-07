51643bdf73fa8a509d9260deebbe841bc60ac9c8
Paolo <paolo.gines@gmail.com>
2025-04-07 19:50:44 +0800

    Created view for recipe maker

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Recipe
+from .forms import RecipeForm
 
 @login_required
 def recipe_list(request):
@@ -11,3 +12,14 @@ def recipe_list(request):
 def recipe(request, key):
     recipe = Recipe.objects.get(id=key)
     return render(request, 'recipe.html', {'recipe': recipe})
+
+@login_required
+def recipe_maker(request):
+    recipe_form = RecipeForm()
+    if request.method == 'POST':
+        recipe_form = RecipeForm(request.POST)
+        if recipe_form.is_valid():
+            recipe_form.instance.author = request.user.profile
+            recipe_form.save()
+            return redirect('ledger:recipe_list')
+    return render(request, 'recipe_maker.html', {'recipe_form':recipe_form})
